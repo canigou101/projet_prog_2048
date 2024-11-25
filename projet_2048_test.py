dae9cff8c1cba65e720d3213cd5767abd05c4f75
canigou101 <127309239+canigou101@users.noreply.github.com>
2024-11-25 09:46:16 +0100

    Delete projet_2048_test.py

--- projet_2048_test.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from  projet_2048 import *
-
-def test_create_grid():
-    assert create_grid(5)==[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-  
